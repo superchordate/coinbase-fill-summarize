643450d15ffad2b133826f4906da726723c0a89f
Superchordate <superchordate@gmail.com>
2020-02-22 15:34:10 -0600

    Add duplicate row check.

--- scripts/1-read.R
+++ scripts/1-read.R
@@ -6,6 +6,15 @@ dt = bind_rows(lapply(
     arrange( created.at )
 ))
 
+# it's possible overlapping files may have 
+dtuniq = dt %>% distinct()
+if(nrow(dtuniq) != nrow(dt)) warning(glue('
+  Dropped {nrow(dt)-nrow(dtuniq)} duplicated rows. Your files may use overlapping dates.
+'))
+dt = dtuniq
+rm(dtuniq)
+
+
 buy = dt %>% filter( side == 'BUY' ) %>% select( -side )
 sell = dt %>% filter( side == 'SELL') %>% select( -side )
 
