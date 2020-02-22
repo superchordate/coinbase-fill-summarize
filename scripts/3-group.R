a93556bdd0d0a3f478f3fa4e57ae76d5056201c2
Superchordate <superchordate@gmail.com>
2020-02-22 15:52:20 -0600

    Add indicator for year or more.

--- scripts/3-group.R
+++ scripts/3-group.R
@@ -3,5 +3,6 @@ gtx = tx %>%
   group_by(year = year(sell.at), sym = buy.sym, buy.at, sell.at) %>% 
   summarize_at( vars(buy, sell, buy_units, sell_units), list(sum) ) %>% ungroup() %>%
   mutate_at( vars(buy, sell), list(round), 0 ) %>%
+  mutate(term=ifelse(ydiff(buy.at,sell.at)>0, 'Year or More', 'Less than a Year')) %>%
   arrange( buy.at ) %>%
   mutate( profit = sell + buy )
