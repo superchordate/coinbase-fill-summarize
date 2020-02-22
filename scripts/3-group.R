e410227cefe5f73f9b62715f5449f907f242299f
Superchordate <superchordate@gmail.com>
2020-02-22 15:59:00 -0600

    Rearrange columns to match form 8949

--- scripts/3-group.R
+++ scripts/3-group.R
@@ -3,6 +3,11 @@ gtx = tx %>%
   group_by(year = year(sell.at), sym = buy.sym, buy.at, sell.at) %>% 
   summarize_at( vars(buy, sell, buy_units, sell_units), list(sum) ) %>% ungroup() %>%
   mutate_at( vars(buy, sell), list(round), 0 ) %>%
-  mutate(term=ifelse(ydiff(buy.at,sell.at)>0, 'Year or More', 'Less than a Year')) %>%
+  mutate(term=factor(ifelse(ydiff(buy.at,sell.at)==0, 'Less than a Year', 'Year or More'), levels=c('Less than a Year', 'Year or More'))) %>%
   arrange( buy.at ) %>%
   mutate( profit = sell + buy )
+
+# rearrange columns to match form 8949.
+colorder = c('year', 'term', 'sym', 'buy.at', 'sell.at', 'sell', 'buy', 'profit', 'buy_units', 'sell_units')
+gtx = gtx[, c(colorder, setdiff(colnames(gtx),colorder))]
+gtx %<>% arrange(year, term, sym, buy.at)
